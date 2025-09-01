2f9d2e40bc9c3dd2ff8ed7fcb92c08fd0b779558
2new4u <2new4u.github.io>
2025-09-01 14:58:02 +0100

    Refactor emoji creation logic in createEmojis function for improved readability

--- js/animation.js
+++ js/animation.js
@@ -6,19 +6,20 @@ function createEmojis() {
     { text: "🪙" },
   ];
 
-  emojis.forEach(({ text }) => {
-    const emoji = document.createElement("div");
+  const { text } = emojis[Math.floor(Math.random() * emojis.length)];
+  const emoji = document.createElement("div");
 
-    emoji.classList.add("emoji");
-    emoji.textContent = text;
-    emoji.style.left = Math.random() * 100 + "vw";
-    emoji.style.animationDuration = 3 + Math.random() * 5 + "s";
-    emoji.style.fontSize = 1 + Math.random() * 2 + "rem";
-    document.body.appendChild(emoji);
+  emoji.classList.add("emoji");
+  emoji.textContent = text;
+  emoji.style.left = Math.random() * 100 + "vw";
+  emoji.style.animationDuration = 3 + Math.random() * 5 + "s";
+  fontSize = 1 + Math.random() * 2;
+  emoji.style.fontSize = fontSize + "rem";
+  emoji.style.zIndex = fontSize < 0.5 ? -1 : 1;
+  document.body.appendChild(emoji);
 
-    emoji.addEventListener("animationend", () => {
-      emoji.remove();
-    });
+  emoji.addEventListener("animationend", () => {
+    emoji.remove();
   });
 }
 
